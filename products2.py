12fb191aa5f7ef14342b72bfa4d2d7d9346115eb
peiweiwu <hebetein0330@gmail.com>
2022-02-17 15:52:10 +0800

    add new file

---
+++ products2.py
@@ -0,0 +1,20 @@
+products =[]
+sum_= 0
+while True:
+    print('按q結束紀錄')
+    name = input('請輸入商品名稱:')
+    if name == 'q':
+        break
+    price = int(input('請輸入商品價格:'))
+    products.append([ name, price])
+    sum_ = sum_ + price
+print ( '您總購買',len(products), '件商品')
+print('總共', sum_,'元')
+print('商品清單')
+
+for p in products: #p 小清單
+    print(p[0] , '的價格是', p[1])
+
+with open('products.csv', 'w') as f:
+    for p in products:
+        f.write(p[0] + ','+str(p[1]) + '\n')
