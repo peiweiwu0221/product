e5cde8a17fbdc5a28dbedc4b35cd426d6c55be8f
peiweiwu <hebetein0330@gmail.com>
2022-02-17 21:39:41 +0800

    add new file

--- products2.py
+++ products2.py
@@ -15,6 +15,8 @@ print('商品清單')
 for p in products: #p 小清單
     print(p[0] , '的價格是', p[1])
 
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+    f.write('商品,價格\n ')
     for p in products:
         f.write(p[0] + ','+str(p[1]) + '\n')
+    f.write('總共' +','+str(sum_))
